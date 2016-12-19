3d64bd98fa700d242c6a52aa2b0ece49408c1366
Charles Dunn <dunnc1551@gmail.com>
2016-12-18 19:06:48 -0700

    Add new event listener for upvote button. Not fully functional yet.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,23 @@
 $('.save-btn').on('click', function(e) {
   e.preventDefault();
   getIdea();
+  clearInputs();
 });
+
+$('.idea-section').on('click', '.upvote-btn', function() {
+  console.log(this)
+  var voteStatus = $('.quality-value').text();
+
+  if ($('voteStatus').text('swill')) {
+    $('voteStatus').text('plausible');
+  }
+  //  } else if ($('voteStatus').text('plausible')) {
+  //   $('voteStatus').text('genius');
+  // } else if ($('voteStatus').text('genius')) {
+  //   $('voteStatus').text('genius');
+  // }
+})
+
 
 //Functions
 
@@ -25,8 +41,13 @@ function getIdea() {
   var ideaTitle = $('.user-title').val();
   var ideaBody = $('.user-body').val();
   var userIdea = new Idea(ideaTitle, ideaBody);
-  console.log(userIdea);
   addIdea(ideaTitle, ideaBody);
+  console.log(userIdea)
+}
+
+function clearInputs() {
+  $('.user-title').val('');
+  $('.user-body').val('');
 }
 
 function Idea(title, body) {
